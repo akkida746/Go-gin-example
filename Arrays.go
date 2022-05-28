ca91191d96475928ba2e60abfbe7e8f550255980
Akki <akkida746@gmail.com>
2022-05-28 09:06:14 +0000

    Maps

--- Arrays.go
+++ Arrays.go
@@ -3,11 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Arrays")
-	var a [2]int
-	a[0] = 1
-	fmt.Println(a)
-
+	fmt.Println("Initializing arrays with size and fixed length")
 	var b = [2]int{1, 2} // constant array and non-growable
 	fmt.Println(b)
 	fmt.Println(len(b))
@@ -18,9 +14,15 @@ func main() {
 	c = append(c, 2)
 	c = append(c, 3)
 	c = append(c, 4)
+	c = append(c, 5)
 	fmt.Println(c)
 	fmt.Println("Printing slicing")
 	fmt.Println(c[:2])
 	fmt.Println(c[:1])
 	fmt.Println(c[1:])
+
+	fmt.Println("Arrays to slice")
+	m := c[1:2]
+	fmt.Println(m)
+
 }
---
+++ Maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps")
+	freq := map[int]int{}
+	freq[1]=1
+	freq[1]=2
+	freq[2]=2
+	fmt.Println(freq)
+}
