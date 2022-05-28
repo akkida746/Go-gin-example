c2296d48cab24c6d88f62783c0a5f7c9b554ae54
Akki <akkida746@gmail.com>
2022-05-28 08:55:04 +0000

    slicing

--- Arrays.go
+++ Arrays.go
@@ -5,9 +5,22 @@ import "fmt"
 func main() {
 	fmt.Println("Arrays")
 	var a [2]int
-	a[0] = 1;
+	a[0] = 1
 	fmt.Println(a)
 
-	var b = [2]int{1,2}
+	var b = [2]int{1, 2} // constant array and non-growable
 	fmt.Println(b)
+	fmt.Println(len(b))
+	fmt.Println(b)
+
+	var c []int // growable array as we have not defined size here.
+	c = append(c, 1)
+	c = append(c, 2)
+	c = append(c, 3)
+	c = append(c, 4)
+	fmt.Println(c)
+	fmt.Println("Printing slicing")
+	fmt.Println(c[:2])
+	fmt.Println(c[:1])
+	fmt.Println(c[1:])
 }
