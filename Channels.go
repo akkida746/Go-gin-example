66d47fe43b644ed70e14bab021f092592aebe683
Akki <akkida746@gmail.com>
2022-05-30 07:48:51 +0000

    channels

--- Channels.go
+++ Channels.go
@@ -4,7 +4,7 @@ import ("fmt";"time";"sync")
 
 func main(){
 	wg := sync.WaitGroup{}
-	wg.Add(2)
+	wg.Add(3)
 	ch := make(chan int)
 	go func(){
 		for i:=0;i<3;i++{
@@ -23,6 +23,20 @@ func main(){
 			}
 			fmt.Println(a)
 				time.Sleep(time.Millisecond * 1000)
+		}
+		wg.Done()
+	}()
+
+	data := make(chan int)
+	go func(){
+		for true{
+			fmt.Println("Getting data from channel")
+			a,isOpen := <- data
+			if !isOpen {
+				break
+			}
+			fmt.Println("Getting data: {}", a)
+			time.Sleep(time.Millisecond*1000)
 		}
 		wg.Done()
 	}()
